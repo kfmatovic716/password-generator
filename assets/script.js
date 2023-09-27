0b24b87d1ff5dd1b727e24798e18af030cbb8a7f
Katherine <kfmatovic@icloud.com>
2023-09-27 07:19:44 -0700

    declare variables and add prompts

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,19 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+let numbers = "0123456789";
+//single backslash inserts a (') single quote and double backslash inserts a single backslash
+let spclChars = '!"\'()*+,-./:;<=>?@[\\]^_`{|}~'; 
+
+var chars = alphabet.concat(numbers).concat(spclChars)
+console.log(chars)
+
+let pwCriteria = prompt("Enter password criteria? ")
+let pwLength = prompt("Enter passwordLength: ");
+let charTypes = prompt("Enter type of characters: ")
+
+
+/*
+var generateBtn = document.querySelector("#generate")
 
 // Write password to the #password input
 function writePassword() {
@@ -13,3 +27,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+*/
