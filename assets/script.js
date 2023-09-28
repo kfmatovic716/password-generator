339ef45c047773d4b63c7079bd625a0d7f054c5f
Katherine <kfmatovic@icloud.com>
2023-09-28 06:33:35 -0700

    add comments in js code

--- assets/script.js
+++ assets/script.js
@@ -5,23 +5,39 @@ let upperAlphabet = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 let numbers = [..."0123456789"];
 //single backslash inserts a (') single quote and double backslash inserts a single backslash
 let spclChars = [...'!"\'()*+,-./:;<=>?@[\\]^_`{|}~']; 
-let pwLength=0
-let userChoices=[]
-let result=""
+let pwLength=0;
+let userChoices=[];
+let result="";
+var generateBtn = document.querySelector("#generate")
 
- 
+// event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
+// function that writes password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+
+// function that generates a password 
 function generatePassword (){
 
+  // kicks out all invalid user inputs; function will only execute when user enters a correct input
   while(pwLength<8 || pwLength>128 || pwLength==="" || isNaN(pwLength)){
     pwLength = prompt("Enter password length between 8 to 128 characters: ");
   }
 
-  var lowerCase = confirm("Do you want lowecase characters?")
+  // user confirms their choice of characters in pw
+  var lowerCase = confirm("Do you want lowecase characters?");
   var upperCase = confirm("Do you want uppercase characters? ");
   var numeric = confirm("Do you want numeric characters?");
   var spclCharacters = confirm("Do you want special characters? ");
 
+  /* conditions on which characters need to be concatenated after user confirms choices;
+  at least 1 characer type should be selected */
   if(lowerCase){
     userChoices=userChoices.concat(lwrAlphabet)
   }
@@ -35,35 +51,25 @@ function generatePassword (){
     userChoices= userChoices.concat(spclChars)
   }
   console.log(userChoices);
+  
 
+  // alert when the user did not select at least one character type
   if(!lowerCase && !upperCase && !numeric && !spclCharacters){
     alert("You need at least one type of character to generate a password")
     window.location.reload()
   }
 
+  // generates an array of characters based on user selected password length
   for (let i = 0; i < pwLength; i++) {
    result+= userChoices[Math.floor(Math.random() * userChoices.length)]
     
   }
 
-return result
-
+return result;
 }
 
 
 
-var generateBtn = document.querySelector("#generate")
-//Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
- //Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
 
